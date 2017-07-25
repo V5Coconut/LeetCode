cac5372f750159a15bf9340572ad6fce89b2d253
V5Coconut <137106716@qq.com>
2017-07-25 17:35:26 +0800

    2

--- LeetCode/src/com/leetcode/ReverseInteger.java
+++ LeetCode/src/com/leetcode/ReverseInteger.java
@@ -1,5 +1,12 @@
 package com.leetcode;
 
 public class ReverseInteger {
-
+	public static void main(String[] args) {
+		reverse(123);
+	}
+	
+	 public static int reverse(int x) {
+		 
+		 return 0;
+	 }
 }
